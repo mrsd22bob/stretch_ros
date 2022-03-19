a36d7feac4de427b23e5e51064cbf18bba5c48c2
SMB <dozoyoroshiku@hotmail.com>
2022-03-18 20:32:16 -0700

    Added manipulation PR3

---
+++ ifa_teamg/nodes/manipulation_pr3.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+from cv2 import transform
+import rospy
+from std_msgs.msg import String
+from std_srvs.srv import Trigger, TriggerRequest, TriggerResponse
+from sensor_msgs.msg import JointState
+from control_msgs.msg import FollowJointTrajectoryGoal
+from trajectory_msgs.msg import JointTrajectoryPoint
+
+import tf2_ros
+import tf2_geometry_msgs
+from tf2_geometry_msgs import PoseStamped 
+
+import hello_helpers.hello_misc as hm
+
+class Manipulation_pr3(hm.HelloNode):
+   
+    def __init__(self):
+        hm.HelloNode.__init__(self)
+        self.tf_buffer = None
+        self.tf_listener = None
+	
+        #self.wrist_sub = rospy.Subscriber("/aruco_single/pose", PoseStamped, self.moveWristToMarker, queue_size=1)
+	
+	
+    #def turnOnManipulationMode(self):
+    #    pass
+
+    def moveWristToMarker(self, pose):
+        
+
+        # transform 'aruco_frame:base_link' to 'base_link'
+        target_frame = 'base_link'
+        print(pose.header.frame_id[1:])
+
+        transform = self.tf_buffer.lookup_transform(target_frame, pose.header.frame_id[1:], pose.header.stamp)
+        pose_transformed = tf2_geometry_msgs.do_transform_pose(pose, transform)
+
+        # returns trans:xyz orien:xyzw
+        offset_y = -0.151
+        offset_z = 0.185 #0.250
+        print(pose_transformed)
+        pose_z = pose_transformed.pose.position.z - offset_z
+        pose_y = -(pose_transformed.pose.position.y - offset_y)
+
+        pose_desired = {'joint_lift' : pose_z, 'wrist_extension' : pose_y }
+        print(pose_desired)
+        self.move_to_pose(pose_desired)
+        #self.trajectory_client.wait_for_result()
+
+    # def moveToolToMarker(poses):
+    #   pass
+
+    def main(self):
+        #rospy.init_node("manipulation_pr3", anonymous=True)
+        hm.HelloNode.main(self, 'manipuation_pr3', 'manipulation_pr3', wait_for_first_pointcloud=False)
+	self.tf_buffer = tf2_ros.Buffer(rospy.Duration(10))
+	self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
+ 
+
+if __name__ == '__main__':
+    try:
+        node = Manipulation_pr3()      
+        node.main()       
+	wrist_sub = rospy.Subscriber("/aruco_single/pose", PoseStamped, node.moveWristToMarker)
+	rospy.spin()
+
+    except KeyboardInterrupt:
+        rospy.loginfo('Interrupt received!')
