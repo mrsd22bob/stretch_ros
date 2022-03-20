4541173c9e5364f9443bdb4c5940e426dfe4d4d4
SMB <dozoyoroshiku@hotmail.com>
2022-03-19 17:16:53 -0700

    works - little modification in offset needed

--- ifa_teamg/nodes/manipulation_pr3.py
+++ ifa_teamg/nodes/manipulation_pr3.py
@@ -16,54 +16,57 @@ import hello_helpers.hello_misc as hm
 
 class Manipulation_pr3(hm.HelloNode):
    
-    def __init__(self):
-        hm.HelloNode.__init__(self)
-        self.tf_buffer = None
-        self.tf_listener = None
-	self.wrist_sub = rospy.Subscriber("/aruco_single/pose", PoseStamped, self.moveWristToMarker, queue_size=1)
-	
-	
-    #def turnOnManipulationMode(self):
-    #    pass
+	def __init__(self):
+		hm.HelloNode.__init__(self)
+		self.pose = None
 
-    def moveWristToMarker(self, pose):
-        
+	#def turnOnManipulationMode(self):
+	#    pass
 
-        # transform 'aruco_frame:base_link' to 'base_link'
-        target_frame = 'base_link'
-        print(pose.header.frame_id[1:])
+	# def move_to_initial_configuration(self):
+	# 	initial_pose = {'wrist_extension': 0.01,
+	# 					'joint_lift': 0.5,}
 
-        transform = self.tf_buffer.lookup_transform(target_frame, pose.header.frame_id[1:], pose.header.stamp)
-        pose_transformed = tf2_geometry_msgs.do_transform_pose(pose, transform)
+	# 	rospy.loginfo('Move to the initial position.')
+	# 	self.move_to_pose(initial_pose)
 
-        # returns trans:xyz orien:xyzw
-        offset_y = -0.151
-        offset_z = 0.185 #0.250
-        print(pose_transformed)
-        pose_z = pose_transformed.pose.position.z - offset_z
-        pose_y = -(pose_transformed.pose.position.y - offset_y)
 
-        pose_desired = {'joint_lift' : pose_z, 'wrist_extension' : pose_y }
-        print(pose_desired)
-        self.move_to_pose(pose_desired)
-        #self.trajectory_client.wait_for_result()
+	def moveWristToMarker(self, pose):
+		
+		# transform 'aruco_frame:base_link' to 'base_link'
+		target_frame = 'base_link'
+		print(pose.header.frame_id[1:])
 
-    # def moveToolToMarker(poses):
-    #   pass
+		transform = self.tf_buffer.lookup_transform(target_frame, pose.header.frame_id[1:], pose.header.stamp)
+		pose_transformed = tf2_geometry_msgs.do_transform_pose(pose, transform)
 
-    def main(self):
-        #rospy.init_node("manipulation_pr3", anonymous=True)
-        hm.HelloNode.main(self, 'manipuation_pr3', 'manipulation_pr3', wait_for_first_pointcloud=False)
-	self.tf_buffer = tf2_ros.Buffer(rospy.Duration(10))
-	self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
- 
+		# returns trans:xyz orien:xyzw
+		offset_y = -0.151
+		offset_z = 0.185 #0.250
+		print(pose_transformed)
+		pose_z = pose_transformed.pose.position.z - offset_z
+		pose_y = -(pose_transformed.pose.position.y - offset_y)
+
+		pose_desired = {'joint_lift' : pose_z, 'wrist_extension' : pose_y }
+		print(pose_desired)
+		self.move_to_pose(pose_desired)
+		
+	# def moveToolToMarker(poses):
+	#   pass
+
+	def main(self):
+		#rospy.init_node("manipulation_pr3", anonymous=True)
+		hm.HelloNode.main(self, 'manipuation_pr3', 'manipulation_pr3', wait_for_first_pointcloud=False)
+		self.tf_buffer = tf2_ros.Buffer(rospy.Duration(10))
+		self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
+		self.wrist_sub = rospy.Subscriber("/aruco_simple/pose", PoseStamped, self.moveWristToMarker, queue_size=1)
 
 if __name__ == '__main__':
-    try:
-        node = Manipulation_pr3()      
-        node.main()       
-	#wrist_sub = rospy.Subscriber("/aruco_single/pose", PoseStamped, node.moveWristToMarker)
-	rospy.spin()
+	try:
+		node = Manipulation_pr3()      
+		node.main()       
+		# wrist_sub = rospy.Subscriber("/aruco_single/pose", PoseStamped, node.moveWristToMarker)
+		rospy.spin()
 
-    except KeyboardInterrupt:
-        rospy.loginfo('Interrupt received!')
+	except KeyboardInterrupt:
+		rospy.loginfo('Interrupt received, so shutting down')
