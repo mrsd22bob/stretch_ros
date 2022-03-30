91e1724011632f8e6c6f48dd5e62248ec0456f43
SMB <dozoyoroshiku@hotmail.com>
2022-03-29 20:29:37 -0700

    Aruco ID 49 works. Height control calibrated

--- ifa_teamg/nodes/manipulation_pr3.py
+++ ifa_teamg/nodes/manipulation_pr3.py
@@ -61,11 +61,11 @@ class Manipulation_pr3(hm.HelloNode):
 	# 	self.trajectory_client.wait_for_result()
 	# # then, the robot needs to stop. 
 
-	def moveWristToMarker(self, pose):
+	def moveWristToBoundingBox(self, pose):
 
 		self.reach = False
-		self.offset_y = -0.230 #changing this to positive may be a way to calibrate the offset. 
-		self.offset_z = 0.122#0.160#0.210 #0.185 #0.250 #robot's arm is lower than the center of aruco marker.. why?
+		self.offset_y = -0.150 #changing this to positive may be a way to calibrate the offset. 
+		self.offset_z = -0.250 #-0.122#0.160#0.210 #0.185  #robot's arm is lower than the center of aruco marker.. why?
 		
 		# transform 'aruco_frame:base_link' to 'base_link'-> might want to try link_lift? no
 		target_frame = 'base_link' 
@@ -73,9 +73,12 @@ class Manipulation_pr3(hm.HelloNode):
 
 		transform = self.tf_buffer.lookup_transform(target_frame, pose.header.frame_id[1:], pose.header.stamp)
 		pose_transformed = tf2_geometry_msgs.do_transform_pose(pose, transform)
-		# print(pose_transformed)
+		print(pose_transformed)
 		# make threshold for the pose to check if it should be updated as a new pose or not 
-		pose_z = pose_transformed.pose.position.z - self.offset_z
+
+
+	
+		pose_z = (pose_transformed.pose.position.z + self.offset_z)
 		pose_y = -(pose_transformed.pose.position.y - self.offset_y)
 
 		pose_arm_retract = {'wrist_extension' : 0.0}
@@ -108,8 +111,8 @@ class Manipulation_pr3(hm.HelloNode):
 		hm.HelloNode.main(self, 'manipulation_pr3', 'manipulation_pr3', wait_for_first_pointcloud=False)
 		self.tf_buffer = tf2_ros.Buffer(rospy.Duration(10))
 		self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
-		# self.wrist_sub = rospy.Subscriber("BoundingBoxTopic", PoseStamped, self.moveWristToBoundingBox, queue_size=1)
-		self.wrist_sub = rospy.Subscriber("/aruco_simple/pose", PoseStamped, self.moveWristToMarker, queue_size=1)
+		# self.wrist_sub = rospy.Subscriber("BoundingBoxTopic", PoseStamped, self.moveWristToMarker, queue_size=1)
+		self.wrist_sub = rospy.Subscriber("/aruco_simple/pose", PoseStamped, self.moveWristToBoundingBox, queue_size=1)
 
 
 
